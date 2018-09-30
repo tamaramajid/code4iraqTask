68ae0556cb12611c7117980b4ada75ac7ce8e24c
tamaramajid <tamaramajid97@gmail.com>
2018-09-30 22:01:38 +0300

    code for iraq

---
+++ Task/main.js
@@ -0,0 +1,5 @@
+document.getElementById("lbtn1").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Thank for joining with us, Welcome to Our big iraqi family , we will send to you Email with details";
+}
